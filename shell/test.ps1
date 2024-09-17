3637724244b8bcb768dae89a7a28d1ee06c3c6fe
yude268 <yude268@qq.com>
2024-09-17 11:39:15 +0800

    Signed-off-by: yude268 <yude268@qq.com>

--- shell/test.ps1
+++ shell/test.ps1
@@ -1,9 +1,8 @@
 # 获取当前路径
 $currentPath = Get-Location
 
-# 定义文件路径
-$exeFile = Join-Path $currentPath "7z.exe"
-$dllFile = Join-Path $currentPath "7z.dll"
+
+$passWord = "88888888"
 
 # 定义下载链接
 $exeUrl = "https://gitee.com/dylanbai8/download/releases/download/27.77/7z.exe"
@@ -19,39 +18,45 @@ if ($files.Count -eq 1) {
     # 如果文件数为1，将文件名赋值给变量
     $fileName = $files.Name
     Write-Output "找到的文件: $fileName"
+Extract-With7z -fileName $fileName -passWord $passWord -exeUrl $exeUrl -dllUrl $dllUrl
+
 } else {
     # 如果文件数不为1，则报错
-    Write-Error "错误: 找到的文件数为 $($files.Count)，应为1个"
+    Write-Output "错误: 找到的文件数为 $($files.Count)，应为1个"
 return
 }
 
 
+function Extract-With7z {
+    param (
+        [string]$fileName,
+        [string]$passWord,
+        [string]$exeUrl,
+        [string]$dllUrl
+    )
 
-# 7z.exe下载
-Invoke-WebRequest -Uri $exeUrl -OutFile $exeFile
-Invoke-WebRequest -Uri $dllUrl -OutFile $dllFile
+    $currentPath = Get-Location
+    $exeFile = Join-Path $currentPath "7z.exe"
+    $dllFile = Join-Path $currentPath "7z.dll"
+    $fileToExtract = Join-Path $currentPath $fileName
 
-# 检查文件是否存在
-$exeExists = Test-Path $exeFile
-$dllExists = Test-Path $dllFile
+    # 下载 7z.exe 和 7z.dll
+    Invoke-WebRequest -Uri $exeUrl -OutFile $exeFile
+    Invoke-WebRequest -Uri $dllUrl -OutFile $dllFile
 
+    # 检查文件是否存在
+    if (Test-Path $exeFile -and Test-Path $dllFile) {
+        attrib +h $exeFile
+        attrib +h $dllFile
 
-if ($exeExists -and $dllExists) {
-    attrib +h $exeFile
-    attrib +h $dllFile
-} else {
-    Write-Output "下载失败或文件不完整，请检查网络连接。"
-}
+        # 解压命令
+        & $exeFile x $fileToExtract "-p$passWord" "-o$currentPath"
+        Write-Output "解压完成。"
 
-
-# 定义文件和7z.exe的路径
-$fileToExtract = Join-Path $currentPath $fileName
-
-
-    # 解压命令，使用密码88888888
-    & $exeFile x $fileToExtract "-p88888888" "-o$currentPath"
-
-    Write-Output "解压完成。"
-
+        # 删除临时文件
         Remove-Item $exeFile -Force
         Remove-Item $dllFile -Force
+    } else {
+        Write-Output "下载失败或文件不完整，请检查网络连接。"
+    }
+}
